26cd1fc621336b453b23ccf90d7d08745a9c1fdd
Abhisek Maiti <digital-idiot@users.noreply.github.com>
2019-01-17 12:58:04 +0530

    Update setup.py
    Updated `setup.py` according to the new changes

--- setup.py
+++ setup.py
@@ -9,10 +9,11 @@ PACKAGES = [
 def setup_package():
     setup(
         name="rotation_forest",
-        version='0.1.0',
+        version='0.2',
         description='Sklearn style implementation of the Rotation Forest Algorithm',
-        author='Joshua D. Loyal',
-        url='https://github.com/joshloyal/RotationForest',
+        author='Joshua D. Loyal, Abhisek Maiti',
+        author_email='mail2abhisek.maiti@gmail.com'
+        url='https://github.com/digital-idiot/RotationForest',
         license='MIT',
         install_requires=['numpy', 'scipy', 'scikit-learn'],
         packages=PACKAGES,
