9ee334e68907e9fd5047c23be85bef894b3e84b9
Abhisek Maiti <digital-idiot@users.noreply.github.com>
2019-01-17 14:02:36 +0530

    Update setup.py
    Added description

--- setup.py
+++ setup.py
@@ -11,8 +11,13 @@ def setup_package():
         name="rotation_forest",
         version='0.2',
         description='Sklearn style implementation of the Rotation Forest Algorithm',
+        long_description='''
+        Implementation of the Algorithm: J. J. Rodriguez, L. I. Kuncheva and C. J. Alonso, "Rotation Forest: A New Classifier Ensemble Method," in IEEE Transactions on Pattern Analysis and Machine Intelligence, vol. 28, no. 10, pp. 1619-1630, Oct. 2006.
+doi: 10.1109/TPAMI.2006.211''',
         author='Joshua D. Loyal, Abhisek Maiti',
-        author_email='mail2abhisek.maiti@gmail.com'
+        author_email='mail2abhisek.maiti@gmail.com',
+        maintainer='Abhisek Maiti',
+        maintainer_email='mail2abhisek.maiti@gmail.com',
         url='https://github.com/digital-idiot/RotationForest',
         license='MIT',
         install_requires=['numpy', 'scipy', 'scikit-learn'],
